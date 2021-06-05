4878c5ecf9fc86efb13b6884ac25dc1a3a3d190f
Daniel Galdino <danielgaldino1712@gmail.com>
2021-06-05 18:11:11 -0300

    basic structure numbers and operators

--- src/app/button/button-operator/button-operator.component.ts
+++ src/app/button/button-operator/button-operator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button-operator',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-operator.component.css']
 })
 export class ButtonOperatorComponent implements OnInit {
+
+  @Input() operator: string = ''
 
   constructor() { }
 
